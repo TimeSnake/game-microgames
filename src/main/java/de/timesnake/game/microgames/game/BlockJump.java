b5d5d44c9456df5a1c73d4b20c6090d715eed1fd
funzter <cavehive@protonmail.com>
2023-02-12 14:51:30 +0100

    minor

--- src/main/java/de/timesnake/game/microgames/game/BlockJump.java
+++ src/main/java/de/timesnake/game/microgames/game/BlockJump.java
@@ -22,7 +22,7 @@ public class BlockJump extends LocationFinishGame implements Listener {
 
     private static final Integer ARENA_SIZE = 50;
     private static final Material PLATFORM = Material.GREEN_CONCRETE;
-    private static final double DENSITY = 0.37;
+    private static final double DENSITY = 0.38;
     private static final int HEIGHT = 50;
 
     private final Random random = new Random();
