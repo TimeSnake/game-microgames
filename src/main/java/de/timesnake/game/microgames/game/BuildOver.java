3a994d915437680a27b63b3e0616d2d424cb2579
funzter <cavehive@protonmail.com>
2024-05-17 21:43:26 +0200

    add game BuildOver

--- src/main/java/de/timesnake/game/microgames/game/BuildOver.java
+++ src/main/java/de/timesnake/game/microgames/game/BuildOver.java
@@ -5,20 +5,32 @@
 package de.timesnake.game.microgames.game;
 
 import de.timesnake.basic.bukkit.util.Server;
+import de.timesnake.basic.bukkit.util.user.event.UserBlockPlaceEvent;
+import de.timesnake.basic.bukkit.util.user.event.UserDeathEvent;
 import de.timesnake.basic.bukkit.util.user.event.UserMoveEvent;
 import de.timesnake.basic.bukkit.util.user.event.UserRespawnEvent;
 import de.timesnake.basic.bukkit.util.user.inventory.ExItemStack;
+import de.timesnake.basic.bukkit.util.world.ExLocation;
+import de.timesnake.basic.bukkit.util.world.ExPolygon;
+import de.timesnake.basic.game.util.game.Map;
 import de.timesnake.game.microgames.game.basis.LocationFinishGame;
+import de.timesnake.game.microgames.main.GameMicroGames;
 import de.timesnake.game.microgames.user.MicroGamesUser;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 
-public class BuildOver extends LocationFinishGame {
+public class BuildOver extends LocationFinishGame implements Listener {
 
   private static final Material BUILDING_BLOCKS = Material.WHITE_WOOL;
   private static final ExItemStack BUILDING_ITEMS = new ExItemStack(BUILDING_BLOCKS, 64);
+
+  private HashMap<Map, ExPolygon> polygonByMap;
 
   public BuildOver() {
     super("build_over", "Build Over",
@@ -27,13 +39,47 @@ public class BuildOver extends LocationFinishGame {
         List.of("§hGoal: §pfirst at finish", "Reach the finish (beacon) at first."),
         1,
         Duration.ofMinutes(3));
+
+    Server.registerListener(this, GameMicroGames.getPlugin());
+  }
+
+  @Override
+  public void beforeMapLoad() {
+    super.beforeMapLoad();
+
+    this.polygonByMap = new HashMap<>();
+  }
+
+  @Override
+  public void onMapLoad(Map map) {
+    super.onMapLoad(map);
+
+    this.polygonByMap.put(map, new ExPolygon(map.getWorld(), map.getLocationsSorted(10, 100)));
+  }
+
+  @Override
+  public void load() {
+    super.load();
+
+    this.currentMap.getWorld().setPVP(true);
   }
 
   @Override
   protected void applyBeforeStart() {
     super.applyBeforeStart();
 
-    Server.getPreGameUsers().forEach(u -> u.addItem(BUILDING_ITEMS.cloneWithId()));
+    Server.getPreGameUsers().forEach(u -> {
+      u.addItem(BUILDING_ITEMS.cloneWithId());
+      u.addItem(BUILDING_ITEMS.cloneWithId());
+    });
+  }
+
+  @Override
+  public void start() {
+    super.start();
+
+    Server.getInGameUsers().forEach(u -> u.setGameMode(GameMode.SURVIVAL));
+    this.currentMap.getWorld().setPVP(true);
   }
 
   @Override
@@ -47,10 +93,32 @@ public class BuildOver extends LocationFinishGame {
   protected void onUserRepsawn(UserRespawnEvent e) {
     super.onUserRepsawn(e);
     e.getUser().addItem(BUILDING_ITEMS.cloneWithId());
+    e.getUser().addItem(BUILDING_ITEMS.cloneWithId());
   }
 
   @Override
   public boolean hasSideboard() {
     return false;
+  }
+
+  @EventHandler
+  public void onUserDeath(UserDeathEvent e) {
+    if (!this.isGameRunning()) {
+      return;
+    }
+
+    e.setDrops(List.of());
+  }
+
+  @EventHandler
+  public void onUserBlockPlace(UserBlockPlaceEvent e) {
+    if (!this.isGameRunning()) {
+      return;
+    }
+
+    if (!this.polygonByMap.get(this.currentMap).contains(ExLocation.fromLocation(e.getBlock().getLocation()))) {
+      e.setCancelled(true);
+      e.setBuild(false);
+    }
   }
 }
