73e2e75864d41958beaa43dc8c3dedba35e4de18
funzter <cavehive@protonmail.com>
2024-05-17 21:41:53 +0200

    minor

--- src/main/java/de/timesnake/game/microgames/chat/Plugin.java
+++ src/main/java/de/timesnake/game/microgames/chat/Plugin.java
@@ -4,17 +4,11 @@
 
 package de.timesnake.game.microgames.chat;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin MICRO_GAMES = new Plugin("MicroGames", "GMG",
-      LogHelper.getLogger("MicroGames", Level.INFO));
+  public static final Plugin MICRO_GAMES = new Plugin("MicroGames", "GMG");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
