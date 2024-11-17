fa453933e9d4c39ab08c3669956b957609243c46
funzter <funzter@mail.timesnake.de>
2024-11-17 01:28:24 +0100

    :alien: update for tablist api changes

--- src/main/java/de/timesnake/game/microgames/user/TablistManager.java
+++ src/main/java/de/timesnake/game/microgames/user/TablistManager.java
@@ -8,6 +8,7 @@ import de.timesnake.basic.bukkit.core.user.scoreboard.tablist.Tablist2;
 import de.timesnake.basic.bukkit.util.Server;
 import de.timesnake.basic.bukkit.util.group.DisplayGroup;
 import de.timesnake.basic.bukkit.util.user.scoreboard.ScoreboardManager;
+import de.timesnake.basic.bukkit.util.user.scoreboard.Tablist;
 import de.timesnake.basic.bukkit.util.user.scoreboard.TablistGroup;
 import de.timesnake.basic.bukkit.util.user.scoreboard.TablistGroupType;
 import de.timesnake.library.chat.ExTextColor;
@@ -58,6 +59,7 @@ public class TablistManager {
 
     this.tablist = Server.getScoreboardManager()
         .registerTablist(new Tablist2.Builder("micro_games")
+            .type(Tablist.Type.INTEGER)
             .groupTypes(types)
             .colorGroupType(de.timesnake.basic.game.util.game.TablistGroupType.GAME_TEAM)
             .addDefaultGroup(de.timesnake.basic.game.util.game.TablistGroupType.GAME_TEAM, this.spectatorGroup)
